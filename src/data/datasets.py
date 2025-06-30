6c16fc2c7326486ff274315083d2ebfd90306473
DongWon Moon <ansehddnjs187@gmail.com>
2025-06-30 10:12:00 +0900

    Utils associate with Data

---
+++ src/data/datasets.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+
+
+class CustomDataset(Dataset):
+    def __init__(self, df: pd.DataFrame):
+        self.users = torch.tensor(df["userId"].values, dtype=torch.long)
+        self.movies = torch.tensor(df["movieId"].values, dtype=torch.long)
+        self.ratings = torch.tensor(df["rating"].values, dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.users)
+
+    def __getitem__(self, index):
+        return self.users[index], self.movies[index], self.ratings[index]
---
+++ src/data/preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+def mapping_id_to_unique(df: pd.DataFrame, target_df: pd.DataFrame):
+    """
+    Id(UserId, MovieId)를 임베딩의 입력으로 준비.
+
+    Args:
+        df (DataFrame): 전체 데이터프레임
+        target_df: 매핑 로직을 적용할 데이터프레임
+    """
+    unique_users = df["userId"].unique()
+    unique_movies = df["movieId"].unique()
+
+    # Mapper
+    user_to_idx = {original: new for new, original in enumerate(unique_users)}
+    movie_to_idx = {original: new for new, original in enumerate(unique_movies)}
+
+    num_users = len(unique_users)
+    num_movies = len(unique_movies)
+
+    target_df["userId"] = target_df["userId"].map(user_to_idx)
+    target_df["movieId"] = target_df["movieId"].map(movie_to_idx)
+
+    target_df.dropna(inplace=True)
+    target_df["userId"] = target_df["userId"].astype(int)
+    target_df["movieId"] = target_df["movieId"].astype(int)
+
+    return num_users, num_movies, target_df
