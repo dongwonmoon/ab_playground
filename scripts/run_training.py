450d93dd3cc4232a10c9d00c4ae542826867d568
DongWon Moon <ansehddnjs187@gmail.com>
2025-06-30 10:10:51 +0900

    Code Clean Up

--- scripts/run_training.py
+++ scripts/run_training.py
@@ -11,6 +11,7 @@ import pandas as pd
 from src.utils.config_loader import load_config
 from src.models.ncf import NCF
 from src.models.svd import SVD
+from src.data.preprocessing import mapping_id_to_unique
 
 
 def run_training():
@@ -26,54 +27,35 @@ def run_training():
     print("Loading pre-processed data and creating ID maps...")
 
     full_df = pd.read_parquet(config["data"]["processed_path"])
-    unique_users = full_df["userId"].unique()
-    unique_movies = full_df["movieId"].unique()
-
-    # Mapper
-    user_to_idx = {original: new for new, original in enumerate(unique_users)}
-    movie_to_idx = {original: new for new, original in enumerate(unique_movies)}
-
-    num_users = len(unique_users)
-    num_movies = len(unique_movies)
-
     train_df = pd.read_parquet(config["data"]["train_data_path"])
-    train_df["userId"] = train_df["userId"].map(user_to_idx)
-    train_df["movieId"] = train_df["movieId"].map(movie_to_idx)
 
-    train_df.dropna(inplace=True)
-    train_df["userId"] = train_df["userId"].astype(int)
-    train_df["movieId"] = train_df["movieId"].astype(int)
+    num_users, num_movies, train_df = mapping_id_to_unique(full_df, train_df)
 
     print(f"Number of users for Embedding: {num_users}")
     print(f"Number of movies for Embedding: {num_movies}")
     print(f"Training data size: {len(train_df)}")
 
     # 모델 학습
-    with mlflow.start_run(run_name="NCF"):
-        ncf_config = config["models"]["ncf"]
-        model_ncf = NCF(
-            params=ncf_config["params"],
-            num_users=num_users,
-            num_movies=num_movies,
-        )
+    mlflow.set_experiment(mlflow_config["experiment_name"])
+    print("\n--- Starting Training and Logging ---")
 
-        model_ncf.train_model(train_df)
+    for model_name in config["models"]:
+        with mlflow.start_run(run_name=model_name.upper()):
+            model_config = config["models"][model_name]
+            if model_name == "ncf":
+                model = NCF(
+                    params=model_config["params"],
+                    num_users=num_users,
+                    num_movies=num_movies,
+                )
+            elif model_name == "svd":
+                model = SVD(params=model_config["params"])
 
-        print("Logging NCF to MLflow...")
-        model_ncf.log_to_mlflow(
-            experiment_name=mlflow_config["experiment_name"], run_name="NCF"
-        )
-
-    with mlflow.start_run(run_name="SVD"):
-        svd_config = config["models"]["svd"]
-        model_svd = SVD(params=svd_config["params"])
-
-        model_svd.train_model(train_df)
-
-        print("Logging SVD to MLflow...")
-        model_svd.log_to_mlflow(
-            experiment_name=mlflow_config["experiment_name"], run_name="SVD"
-        )
+            model.train_model(train_df)
+            print(f"Logging {model_name.upper()} to MLflow...")
+            model.log_to_mlflow(
+                run_name=model_name.upper(),
+            )
 
     print("\n--- Training and Logging Complete! ---")
     print("Check MLflow UI to see the results.")
