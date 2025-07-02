0335bcd16ebbbd21b33cab2eb75f4bf911cea1d4
DongWon Moon <ansehddnjs187@gmail.com>
2025-07-02 14:21:55 +0900

    Column check

--- src/data/datasets.py
+++ src/data/datasets.py
@@ -5,6 +5,12 @@ import pandas as pd
 
 class CustomDataset(Dataset):
     def __init__(self, df: pd.DataFrame):
+        if sorted(["userId", "movieId", "rating"]) != sorted(df.columns):
+            raise ValueError("Column must be userId, movieId, rating.")
+
+        if not df["rating"]:
+            df["rating"] = 0
+
         self.users = torch.tensor(df["userId"].values, dtype=torch.long)
         self.movies = torch.tensor(df["movieId"].values, dtype=torch.long)
         self.ratings = torch.tensor(df["rating"].values, dtype=torch.float32)
